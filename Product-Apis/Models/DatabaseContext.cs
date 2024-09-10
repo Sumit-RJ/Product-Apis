25f5999cfde11bf15a52d0b93ceb4f2e8ae53ff8
sumit <sumit@DESKTOP-4KB4ML7>
2024-09-10 11:40:38 +0530

    Code first instructions

--- Product-Apis/Models/DatabaseContext.cs
+++ Product-Apis/Models/DatabaseContext.cs
@@ -4,7 +4,13 @@ namespace Product_Apis.Models
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options)
+		//Note : For code first migration follow below tutorial & commands
+		// https://medium.com/c-sharp-programming/tutorial-code-first-approach-in-asp-net-core-mvc-with-ef-5baf5af696e9
+
+		// Before running migration command you need to install Nuget package : Microsoft.EntityFrameworkCore.Tools 
+		// the version of above nuget should be same as Microsoft.EntityFrameworkCore which is already istalled. 
+
+		public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options)
         {
             
         }
